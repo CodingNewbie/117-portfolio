6357cfec95665fbfa1f18c557d3c5218ad72c419
Adam <adamrayschmidt@gmail.com>
2024-06-29 09:29:19 -0700

    Add functionality to add portfolio items .

--- content/views.py
+++ content/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
 
 def projects_list(request):
-    return render(request, 'content/projects_list.html')
+    projects = Project.objects.all()
+    return render(request, 'content/projects_list.html', {"projects": projects})
+
+def project_new(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save()
+            return redirect('projects_list')
+
+    else:
+        form = ProjectForm()
+
+    form = ProjectForm()
+    return render(request, 'content/projects_new.html', {"form": form})
